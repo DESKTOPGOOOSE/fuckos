503025650b777668e8474654b746bc529d6f950e
Longhorn5048 <77574002+DESKTOPGOOOSE@users.noreply.github.com>
2021-05-03 10:54:34 -0700

    Update en-us.h

--- localisation/en-us.h
+++ localisation/en-us.h
@@ -1,3 +1,3 @@
-char welcome_en[] = "Welcome to ";
+char welcome_en[] = "WELCUM TO FUCK OS SDFNASNKDNKFNKFSNKDFNKASDNKFNK";
 char helpmessage_en[] = "All commands in this OS as of this version!\n/*-----------------------------------------*/\nCLS          : Clears the screen!\nPAGE         : Shows Page info. For debugging purpose only!\nHALT         : Halts the CPU!\nABOUT        : Shows information about the OS\nCOLOR        : Shows the colors supported by the vga driver\nDIE          : Crashes the system\n"
 // Add more stuff here. (cant as of now)
